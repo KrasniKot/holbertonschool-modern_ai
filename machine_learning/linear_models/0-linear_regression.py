04000e15cb9aec03d2da907ec7c2f7768ce643bb
KrasniKot <emanueltrias9@gmail.com>
2026-03-04 22:22:55 +0000

    task 0, linear regression, finished

---
+++ machine_learning/linear_models/0-linear_regression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env
+
+""" This module contains Linear_Regression
+"""
+
+from sklearn.linear_model import LinearRegression
+
+
+def Linear_Regression():
+    """ Function that returns a LinearRegression instance"""
+
+    return LinearRegression()
