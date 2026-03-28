640a543d0322f4d278db66aa0d212ca305f988a8
KrasniKot <emanueltrias9@gmail.com>
2026-03-28 01:27:11 +0000

    task 5 completed

---
+++ machine_learning/linear_models/5-logisitc_regression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" This module contains Logistic_Regression_Model """
+
+from sklearn import linear_model
+
+
+def Logistic_Regression_Model(random_state):
+    """ Creates a logistic regression model using Scikit-learn
+        > random_state ..... An integer used to set the random seed for reproducibility
+        
+        >>> An untrained LogisticRegression instance.
+    """
+    return linear_model.LogisticRegression()
